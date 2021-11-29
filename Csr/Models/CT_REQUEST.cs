63719b3b0c6efd228ee3a2ea35030af4dd756cfc
dhkwak <dreamaboutme.k@gmail.com>
2021-11-29 23:06:27 +0900

    Request 추가 중

---
+++ Csr/Models/CT_REQUEST.cs
@@ -0,0 +1,20 @@
+﻿namespace Csr.Models
+{
+    public class CT_REQUEST : ModelBase
+    {
+        public int REQUEST_ID { get; set; }
+        public RequestType REQUEST_TYPE { get; set; }   //
+        public DateTime CREATE_DT { get; set; }
+        public string PROJECT_ID { get; set; }
+        public string CUSTOMER_USER_ID { get; set; }
+        public string REQUEST_TITLE { get; set; }
+        public string REQUEST_DETAIL { get; set; }
+    }
+
+    public enum RequestType
+    {
+        단순문의,
+        프로그램오류,
+        데이터수정
+    }
+}
