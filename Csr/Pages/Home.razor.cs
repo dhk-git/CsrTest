e27fb48b2b512525e5ec52aa57489d345557766e
dhkwak <dreamaboutme.k@gmail.com>
2021-12-08 23:19:44 +0900

    summernote 적용테스트중

---
+++ Csr/Pages/Home.razor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Csr.Components.Editor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Csr.Pages
+{
+    public partial class Home
+    {
+
+        private BlazingSummerJsInterop _blazingSummerJsInterop;
+        private bool _edit = true;
+
+        [Parameter] public string Content { get; set; }
+
+        [Parameter] public EventCallback<string> ContentChanged { get; set; }
+
+        private string NoteId { get; } = $"BlazingSummerNote{new Random().Next(0, 1000000).ToString()}";
+        
+        private void EditorUpdate(object sender, string editorText)
+        {
+            Content = editorText;
+            ContentChanged.InvokeAsync(editorText);
+        }
+
+        protected override async Task<Task> OnInitializedAsync()
+        {
+            //_blazingSummerJsInterop = new BlazingSummerJsInterop(Js, NoteId, EditorUpdate);
+            //await _blazingSummerJsInterop.Init();
+            return base.OnInitializedAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            //_blazingSummerJsInterop = new BlazingSummerJsInterop(Js, NoteId, EditorUpdate);
+            //await _blazingSummerJsInterop.Init();
+            //return base.OnAfterRenderAsync(firstRender);
+
+            
+
+
+        }
+
+        private async Task Save()
+        {
+            _edit = false;
+            await _blazingSummerJsInterop.Save();
+            StateHasChanged();
+        }
+
+        private async Task Edit()
+        {
+            _edit = true;
+            var response = await _blazingSummerJsInterop.Edit(Content);
+            StateHasChanged();
+        }
+
+    }
+}
--- Csr/Pages/Home.razor.js
+++ Csr/Pages/Home.razor.js
@@ -1,3 +1,11 @@
-﻿export function showAlert(message) {
+﻿
+
+export function showAlert(message) {
     alert(message);
+}
+
+
+export function setSummernote() {
+    //import('js/summernote-0.8.18-dist/summernote.js');
+    document.getElementById('summernote').summernote();
 }
